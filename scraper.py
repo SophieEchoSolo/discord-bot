81359902d92eb81ac41d56f354faf0d1aea40b04
Sophie <53999238+SophieOrgana@users.noreply.github.com>
2020-02-11 15:34:47 -0500

    Changed bot.event

--- scraper.py
+++ scraper.py
@@ -1,15 +1,20 @@
 import os
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+@bot.event
+async def on_message(message):
+    guild = message.guild
+    if guild:
+        path = "chatlogs/{}.txt".format(guild.id)  
+        with open(path, 'a+') as f:
+            print("{0.timestamp} : {0.author.name} : {0.content}".format(message), file=f)
+    await bot.process_commands(message)
 
-client.run(token)
+bot.run(token)
