dedf4370deec6f3934a6c1d4e1bdfe84d43e83e7
SophieOrgana <admin@sophiesolo.biz>
2020-02-23 09:59:16 -0500

    added importing to file

--- scraper.py
+++ scraper.py
@@ -5,6 +5,9 @@ from discord.ext import commands
 from discord.ext.commands import bot
 from dotenv import load_dotenv
 import pymysql.cursors
+import sys
+sys.path.insert(1, '../houseplots/')
+from plots import *
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
